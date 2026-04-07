49967cfa3c9d30986c21f38720cfd6dc4b9e712f
racerfang <racerfang@gmail.com>
2026-04-07 15:33:41 +0000

    Readded changes removed from merge #59

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app = FastAPI(title="MSRP Platform", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["http://localhost:5173", "http://127.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
